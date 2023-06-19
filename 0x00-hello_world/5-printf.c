c5cc0f445987818b1636271379ce6e70b83958c2
Deolahh <deolaabiola@gmail.com>
2023-06-19 03:25:29 -0700

    ggg

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  main - this program prints an output using prinf function
+ *
+ *  Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

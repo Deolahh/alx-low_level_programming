d3a9d89ccdf0a4e20c0367f630b716eedb5cc905
Deolahh <deolaabiola@gmail.com>
2023-06-27 00:01:22 -0700

    7 puts

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifdef main_h
-#define main_h
-
 #include <stdio.h>
 #include <unistd.h>
 
@@ -16,5 +13,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-#endif
